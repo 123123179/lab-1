e064dd8f3cfa61ff06ba56369ad2cc92e98acca0
123123179 <241265725+123123179@users.noreply.github.com>
2025-11-10 05:49:53 +0200

    task 2.py

--- task 2.py
+++ task 2.py
@@ -1,6 +1,13 @@
 import requests
 
-my_response = requests.get('https://bank.gov.ua/NBU_Exchange/exchange_site?json&valcode=usd&start=20251103&end=20251110&sort=exchangedate&order=desc')
+url = 'https://bank.gov.ua/NBU_Exchange/exchange_site?json&valcode=usd&start=20251103&end=20251110&sort=exchangedate&order=desc'
+response = requests.get(url)
 
-print(my_response.json())
-print(my_response)
+if response.status_code == 200:
+    data = response.json()
+
+    print("💵 Курс долара США (USD) за період 03.11.2025 — 10.11.2025:\n")
+    for record in data:
+        date = record['exchangedate']
+        rate = record['rate']
+        print(f"📅 {date}: {rate:.2f} грн")
